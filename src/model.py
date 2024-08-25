27580bdc16af51523c36fa626a40d6059225a8af
BlackRose484 <hungnbc2@gmail.com>
2024-08-25 23:13:19 +0700

    [fix]: error create

--- src/model.py
+++ src/model.py
@@ -53,13 +53,13 @@ class Yolov1(nn.Module):
 
         for x in architecture:
             if type(x) == tuple:
-                layers += CNNBlock(
+                layers += [CNNBlock(
                     in_channels,
                     out_channels=x[1],
                     kernel_size=x[0],
                     stride=x[2],
                     padding=x[3]
-                )
+                )]
 
                 in_channels = x[1]
             elif type(x) == str:
